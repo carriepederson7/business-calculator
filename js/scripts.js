48df443f85d6067eaf133f3a49d0bf9523b9c76f
Carrie Schmid and Mia Caton <mia@>
2019-08-14 14:39:51 -0700

    Add functions and more html to project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,98 @@
-var add = function (number1, number2){return number1 + number2};
-var number1= parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"));
-alert(add(number1,number2));
+// Back-end logic:
 
-var subtract = function (number1, number2){return number1 - number2};
-var number1= parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"));
-alert(subtract(number1,number2));
 
-var multiply = function (number1, number2){return number1 * number2};
-var number1= parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"));
-alert(multiply(number1,number2));
 
-var divide = function (number1, number2){return number1 / number2};
-var number1= parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"));
-alert(divide(number1,number2));
+var add = function (number1, number2){
+return number1 + number2};
+// var newNumber = parseInt($("number1, number2"))};
 
-var bodyMass = function(weightAsk, heightAsk) {
-var height = parseInt(prompt(heightAsk));
-var weight = parseInt(prompt(weightAsk));
-var bmi = weight / (height * height);
-alert("Your bmi is" + bmi)};
 
-bodyMass("what is your weight?", "What is your height?");
+var subtract = function (number1, number2){
+  return number1 - number2};
 
-var farenheittocelcius = function (number1, number2){return number1 / number2};
-var number1= parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"));
-alert(divide(number1,number2));
+// var number1= parseInt(prompt("Enter a number:"));
+// var number2= parseInt(prompt("Enter another number:"));
 
-var farenheittocelcius = function (number1){return (number1 - 32)*(5/9)};
-var number1= parseInt(prompt("Enter temperature in farenheit:"));
-alert(farenheittocelcius(number1));
+
+var multiply = function (number1, number2){
+  return number1 * number2};
+
+// var number1= parseInt(prompt("Enter a number:"));
+// var number2= parseInt(prompt("Enter another number:"));
+
+
+var divide = function (number1, number2){
+  return number1 / number2};
+
+// var number1= parseInt(prompt("Enter a number:"));
+// var number2= parseInt(prompt("Enter another number:"));
+
+
+
+
+
+// User logic:
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  $("#output").text(result);
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#subtract1").val());
+  var number2 = parseInt($("#subtract2").val());
+  var result = subtract(number1, number2);
+  $("#output2").text(result);
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// carrie's Monday extra notes
+// var bodyMass = function(weightAsk, heightAsk) {
+// var height = parseInt(prompt(heightAsk));
+// var weight = parseInt(prompt(weightAsk));
+// var bmi = weight / (height * height);
+// alert("Your bmi is" + bmi)};
+//
+// bodyMass("what is your weight?", "What is your height?");
+//
+// var farenheittocelcius = function (number1, number2){return number1 / number2};
+// var number1= parseInt(prompt("Enter a number:"));
+// var number2= parseInt(prompt("Enter another number:"));
+// alert(divide(number1,number2));
+//
+// var farenheittocelcius = function (number1){return (number1 - 32)*(5/9)};
+// var number1= parseInt(prompt("Enter temperature in farenheit:"));
+// alert(farenheittocelcius(number1));
